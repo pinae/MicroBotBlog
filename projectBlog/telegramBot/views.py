1358e7ee4541f4517acc3f57ef502af43b1ccf25
Pina Merkert <pmk@ct.de>
2021-12-08 16:55:11 +0100

    Do not check for CSRF tokens in requests for the webhook

--- projectBlog/telegramBot/views.py
+++ projectBlog/telegramBot/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.apps import apps
 
 
+@csrf_exempt
 def webhook(request):
     dispatcher = apps.get_app_config('telegramBot').dispatcher
     dispatcher.process_update(request.body)
