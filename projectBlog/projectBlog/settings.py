dc7f28db2a8d33885b19f1e2fb414f1a0d6ffd92
Pina Merkert <pmk@ct.de>
2021-12-10 18:19:22 +0100

    Load the included apps first

--- projectBlog/projectBlog/settings.py
+++ projectBlog/projectBlog/settings.py
@@ -31,15 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'microblog.apps.MicroblogConfig',
-    'markdownify.apps.MarkdownifyConfig',
-    'telegramBot.apps.TelegrambotConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'microblog.apps.MicroblogConfig',
+    'markdownify.apps.MarkdownifyConfig',
+    'telegramBot.apps.TelegrambotConfig',
 ]
 
 MIDDLEWARE = [
