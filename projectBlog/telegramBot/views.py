5a197c978c1f7dcde76798b6782b3fdcf038dac9
Pina Merkert <pmk@ct.de>
2021-12-16 15:44:36 +0100

    Removed unused imports

--- projectBlog/telegramBot/views.py
+++ projectBlog/telegramBot/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.middleware.csrf import get_token
 from django.http import HttpResponse
 from django.apps import apps
 from .bot import UpdateWithToken
-from json import loads, decoder, dumps
+from json import loads, decoder
 
 
 @csrf_exempt
@@ -18,7 +17,9 @@ def webhook(request):
         print("---------------------")
     except decoder.JSONDecodeError:
         return HttpResponse("Error: This is not vaild JSON.", status=500)
-    if "message" in update_data and "from" in update_data["message"] and "is_bot" not in update_data["message"]["from"]:
+    if ("message" in update_data
+            and "from" in update_data["message"]
+            and "is_bot" not in update_data["message"]["from"]):
         update_data["message"]["from"]["is_bot"] = False
     telegram_update = UpdateWithToken.de_json(data=update_data, bot=bot)
     telegram_update.csrf_token = get_token(request)
